c1804472c4834dbe29a237e3c4f030086f0b0631
yyypasserby <nixingwuwei@gmail.com>
2015-01-17 23:07:39 +0800

    add generateThree

--- public.py
+++ public.py
@@ -1,12 +1,30 @@
 import networkx as nx
 import os
+import sys
+import re2nfa as r2n
+import nfa2dfa as n2d
+import min_dfa 
+import io
 
 def storeAsJPG(mg, name='nfa', openow=False):
-    nx.draw_graphviz(mg)
     nx.write_dot(mg, name + '.dot')
     os.system('dot -Tjpg ' + name + '.dot -o ' + name + '.jpg')
     if openow:
         os.system('open ' + name + '.jpg')
+
+def generateThreeFA(re, name):
+    nfa = r2n.re2nfa(re)
+    storeAsJPG(nfa.graph, name + 'nfa')
+    terminals = r2n.getAllTerminals(re)
+
+    transition_table, all_states, acceptable_states = n2d.constructTransitionTable(terminals, nfa)
+    dfa, reverse_table = n2d.constructDFA(transition_table, all_states, acceptable_states)
+    storeAsJPG(dfa.graph, name + 'dfa')
+
+    mindfa, min_reverse_table = min_dfa.constructMinDFA(transition_table, all_states, dfa.first, dfa.last, terminals)
+    storeAsJPG(mindfa.graph, name + 'min_dfa');
+
+
 
 class MyGraph:
     def __init__(self):
